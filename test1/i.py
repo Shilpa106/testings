febcbd63f770b0eabfc66d0b4beb3b6d94361aae
shilpa106 <yshilpa234@gmail.com>
2023-01-25 16:02:49 +0530

    fdsfa

---
+++ test1/i.py
@@ -0,0 +1,82 @@
+f ds
+
+test1/b.py | 12 +++++++++++
+ test1/c.py | 14 ++++++++++++-
+ test1/d.py | 20 +++++++++++++++++-
+ test1/e.py |  6 +++++-
+ test1/f.py | 16 ++++++++++++++
+ test1/g.py | 13 ++++++++++++
+ 7 files changed, 137 insertions(+), 15 deletions(-)
+ create mode 100644 test1/f.py
+ create mode 100644 test1/g.py
+PS C:\Users\lenovo\Documents\testing> git branch
+  da123
+  dev
+* master
+  test
+PS C:\Users\lenovo\Documents\testing> git checkout dev
+Switched to branch '
+test1/b.py | 12 +++++++++++
+ test1/c.py | 14 ++++++++++++-
+ test1/d.py | 20 +++++++++++++++++-
+ test1/e.py |  6 +++++-
+ test1/f.py | 16 ++++++++++++++
+ test1/g.py | 13 ++++++++++++
+ 7 files changed, 137 insertions(+), 15 deletions(-)
+ create mode 100644 test1/f.py
+ create mode 100644 test1/g.py
+PS C:\Users\lenovo\Documents\testing> git branch
+  da123
+  dev
+* master
+  test
+PS C:\Users\lenovo\Documents\testing> git checkout dev
+Switched to branch '
+test1/b.py | 12 +++++++++++
+ test1/c.py | 14 ++++++++++++-
+ test1/d.py | 20 +++++++++++++++++-
+ test1/e.py |  6 +++++-
+ test1/f.py | 16 ++++++++++++++
+ test1/g.py | 13 ++++++++++++
+ 7 files changed, 137 insertions(+), 15 deletions(-)
+ create mode 100644 test1/f.py
+ create mode 100644 test1/g.py
+PS C:\Users\lenovo\Documents\testing> git branch
+  da123
+  dev
+* master
+  test
+PS C:\Users\lenovo\Documents\testing> git checkout dev
+Switched to branch '
+test1/b.py | 12 +++++++++++
+ test1/c.py | 14 ++++++++++++-
+ test1/d.py | 20 +++++++++++++++++-
+ test1/e.py |  6 +++++-
+ test1/f.py | 16 ++++++++++++++
+ test1/g.py | 13 ++++++++++++
+ 7 files changed, 137 insertions(+), 15 deletions(-)
+ create mode 100644 test1/f.py
+ create mode 100644 test1/g.py
+PS C:\Users\lenovo\Documents\testing> git branch
+  da123
+  dev
+* master
+  test
+PS C:\Users\lenovo\Documents\testing> git checkout dev
+Switched to branch '
+test1/b.py | 12 +++++++++++
+ test1/c.py | 14 ++++++++++++-
+ test1/d.py | 20 +++++++++++++++++-
+ test1/e.py |  6 +++++-
+ test1/f.py | 16 ++++++++++++++
+ test1/g.py | 13 ++++++++++++
+ 7 files changed, 137 insertions(+), 15 deletions(-)
+ create mode 100644 test1/f.py
+ create mode 100644 test1/g.py
+PS C:\Users\lenovo\Documents\testing> git branch
+  da123
+  dev
+* master
+  test
+PS C:\Users\lenovo\Documents\testing> git checkout dev
+Switched to branch '
